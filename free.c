d4906e961fc9ed1add84c25debe29a28905d2efe
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-10 02:14:15 -0700

    Create free function for free stack

--- free.c
+++ free.c
@@ -11,3 +11,15 @@ void free_all(stack_t **stack, char *line, FILE *fp)
 	free(line);
 	fclose(fp);
 }
+
+void free(stack_t **stack)
+{
+	stack_t *current;
+
+	while (*stack != NULL)
+	{
+		current = *stack;
+		*stack = (*stack)->next;
+		free(current);
+	}
+}
