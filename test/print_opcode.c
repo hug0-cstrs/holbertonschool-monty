55b5f31764e1031fcdd902f055f4bfe4a4d56e21
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-10 10:52:37 +0200

    fix betty style for print_opcode.c

--- test/print_opcode.c
+++ test/print_opcode.c
@@ -19,13 +19,20 @@ void pall(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 	}
 }
 
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: pointer to stack
+ * @line_number: instruction's line number
+ * Description: This function prints the value at the top of the stack, without
+ * modifying the stack.
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
 	int value;
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr,"L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
 		exit(EXIT_FAILURE);
 	}
 	value = (*stack)->n;
