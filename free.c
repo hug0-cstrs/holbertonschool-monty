12a708728be082099f1d68181b52620827d3eb49
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-10 11:18:04 +0200

    add description for free functions

--- free.c
+++ free.c
@@ -1,7 +1,11 @@
 #include "monty.h"
 /**
- *
- *
+ * free_all - frees all allocated memory
+ * @stack: pointer to stack
+ * @line: pointer to line buffer
+ * @fp: file pointer
+ * Description: This function frees all allocated memory for the stack, line
+ * buffer, and file pointer. It also closes the file.
  */
 
 void free_all(stack_t **stack, char *line, FILE *fp)
@@ -12,7 +16,12 @@ void free_all(stack_t **stack, char *line, FILE *fp)
 	fclose(fp);
 }
 
-void free(stack_t **stack)
+/**
+ * free_stack - frees the top element of the stack
+ * @stack: pointer to stack
+ * Description: This function frees the memory of the top element of the stack
+ */
+void free_stack(stack_t **stack)
 {
 	stack_t *current;
 
