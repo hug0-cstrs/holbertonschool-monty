0615337d0dcc64fa3f7cd99650d07bfa0a5faf34
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-11 00:40:39 -0700

    Move all free functions in the new file free.c

--- help.c
+++ help.c
@@ -23,19 +23,3 @@ int _isdigit(char *c)
 	return (1);
 }
 
-/**
- * cleanStack - Frees mallocs and close de files
- * @stack: Stack
- */
-void cleanStack(stack_t **stack)
-{
-	stack_t *current = *stack;
-
-	for (; current; current = *stack)
-	{
-		*stack = (*stack)->next;
-		free(current);
-	}
-	fclose(global.fd);
-	free(global.line);
-}
