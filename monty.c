38d41a445ac0cd85de516777f74888eac646a172
hug0-cstrs <hugo.casteras@outlook.fr>
2023-05-11 12:01:52 +0200

    create sub function that subtracts the top element of the stack

--- monty.c
+++ monty.c
@@ -85,6 +85,7 @@ int get_opc(stack_t **stack, char *arg, char *item, int count)
 	    {"swap", _swap},
 	    {"add", _add},
 	    {"nop", _nop},
+	    {"sub", _sub},
 	    {NULL, NULL}};
 	/* Parcours le tableau des opcodes pour trouver le bon opcode */
 	while (op[i].opcode)
--- monty.h
+++ monty.h
@@ -69,6 +69,7 @@ void _swap(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *stack);
 void cleanStack(stack_t **stack);
 int _isdigit(char *c);
